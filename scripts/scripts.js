8ec904c5706ef8de09ccb07ac2acb7396e2e8ced
ali-ismail02 <alii.ismail02@gmail.com>
2022-09-16 21:32:44 +0300

    added more functions and added await

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,7 +1,10 @@
+//Couldn't manage the await functions well due to time constriction i need to work on the twitter project
+//Please play slowly and the game should work properly
+//the game will glitcgh out while pressing quickly
+
 const cards = document.querySelectorAll(".temp")
 const scoreH1 = document.getElementById("score")
 const cardDivs = document.querySelectorAll(".card")
-const button = document.getElementById("reset")
 const array = [0,1,2,3,4,5]
 let solved = 0
 let score = 0
@@ -9,61 +12,92 @@ let score = 0
 
 // functions
 
-const flip = () => {
-    for(const i of cardDivs){
-        i.classList.toggle("is-flipped")
+const flip = async() => { // flips all cards
+    for(const z of cardDivs){
+        z.classList.toggle("is-flipped")
     }
 }
 
 
-function sleep (time) { // waits for time = ms
-    return new Promise((resolve) => setTimeout(resolve, time));
-  }
+function delay(milliseconds){ // allows for delay
+    return new Promise(resolve => {
+        setTimeout(resolve, milliseconds);
+    });
+}
 
-const shuffle = () => {
+const shuffle = async () => {
     const array2=array.sort(() => Math.random() - 0.5 ) // shuffled array to use it as indexes of cards
-    for(let i = 0 ; i< array.length ; i++ ){
-        cards[array2[i]].classList.add(`card-${Math.floor(i/2)}`) // added different classes to cards depending on the prev suffled array
+    for(let y = 0 ; y< array.length ; y++ ){
+        if(cards[array2[y]].classList.contains(`card-0`)) cards[array2[y]].classList.remove(`card-0`)
+        if(cards[array2[y]].classList.contains(`card-1`)) cards[array2[y]].classList.remove(`card-1`)
+        if(cards[array2[y]].classList.contains(`card-2`)) cards[array2[y]].classList.remove(`card-2`)
+        cards[array2[y]].classList.add(`card-${Math.floor(y/2)}`) // added different classes to cards depending on the prev suffled array
+        cardDivs[y].classList.toggle("is-flipped")
     }
 }
 
-for(const i of cardDivs){
-    i.addEventListener('click', () => {
-        if(!i.classList.contains("display-none")){
-            i.classList.toggle("is-flipped")
-            for(const j of cardDivs){
-                if(i!=j && j.classList.contains("is-flipped")){// checks for other flipped cards
-                    let flag = 0
-                    for(let x = 0 ; x <3 ; x++) { // checks if they are of the same type
-                        if(j.lastElementChild.classList.contains(`card-${x}`) && i.lastElementChild.classList.contains(`card-${x}`)){
-                            i.classList.add("display-none")
-                            j.classList.add("display-none")
-                            solved++
-                            flag = 1
-                            if(solved == 2){
-                                score++
-                                scoreH1.innerHTML = `Score: ${score}`
+const removeDisplayNone = async () => { // returns all cards back into display
+    for(const i of cardDivs){
+        if(i.classList.contains("display-none")){
+            i.classList.remove("display-none")
+        }
+    }
+}
+
+const boardReset = async () => { // resets the board after a win
+    await delay(500)
+    score++
+    scoreH1.innerHTML = `Score: ${score}`
+    solved = 0
+    shuffle()
+    removeDisplayNone()
+    await delay(3000)
+    flip()
+}
+
+const events = async () => { // adds event listeners 
+    for (const i of cardDivs) {
+        i.addEventListener('click', async () => {
+            if (!i.classList.contains("display-none")) { // allows only visible cars to be pressed
+                i.classList.toggle("is-flipped")
+                for (const j of cardDivs) {
+                    if (i != j && j.classList.contains("is-flipped")) {// checks for other flipped cards
+                        let flag = 0
+                        for (let x = 0; x < 3; x++) { // checks if they are of the same type
+                            if (j.lastElementChild.classList.contains(`card-${x}`) && i.lastElementChild.classList.contains(`card-${x}`)) {
+                                await delay(500)
+                                i.classList.add("display-none")
+                                j.classList.add("display-none")
+                                i.classList.remove("is-flipped")
+                                j.classList.remove("is-flipped")
+                                j.lastElementChild.classList.remove(`card-${x}`)
+                                i.lastElementChild.classList.remove(`card-${x}`)
+                                solved++
+                                if (solved == 3) { // checks if all cards are matched
+                                    boardReset()
+                                }
+                                flag = 1
                             }
                         }
-                    }
-                    if(flag == 0){
-                        sleep(500).then(() => {
+                        if (flag == 0) {// removes 1 from score if they weren't the same and reflips them
+                            await delay(500)
                             i.classList.remove("is-flipped")
                             j.classList.remove("is-flipped")
                             score--
                             scoreH1.innerHTML = `Score: ${score}`
-                        });
+                        }
                     }
                 }
             }
-        }
-    })
+        })
+    }
 }
 
-flip()
-
-sleep(4000).then(() => {
+const flipp  = async () => { // awaits 3 seconds before flipping the cards
+    await delay(3000)
     flip()
-});
+    events()
+}
 
 shuffle()
+flipp()
